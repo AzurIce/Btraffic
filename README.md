fdf4ca32a2a4f2d67d973afc1787659a8532b7e5
Azur冰弦 <973562770@qq.com>
2024-06-04 18:49:36 +0800

    feat: ebi

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
 依赖安装：
 
 ```
-pip install pandas matplotlib openpyxl
+pip install pandas matplotlib openpyxl numpy scipy
 ```
+
+
+
+位置为 $x$ 时速度为 $v_m$
+$$
+\sqrt{-2ax + v(x_1)^2} = v(x_0)\\
+
+x_0 = x_1 - \delta\\
+
+\sqrt{-2a\delta + v(x_1)^2} = v(x_1-\delta)
+$$
+
+
+
 
 
 
--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy.interpolate import interp1d
 
 from typing import List, Tuple
+
+启动加速度 = 0.8 # m/s^2
+牵引加速度 = 0.5 # m/s^2
+紧急制动率 = 1.2 # m/s^2
+常用制动率 = 0.5 # m/s^2
+车长 = 80 # m
+制动建立时延 = 0.7 # s
+牵引切断延时 = 0.7 # s
+旋转质量系数 = 1.08
+ATP余量 = 3 # km/h
+ATO余量 = 5 # km/h
+max_distance = 24000 # m
+max_speed = 87 # km/h
 
 # Load the Excel file using pandas
 station_df = pd.read_excel('线路条件数据.xlsx', sheet_name='station')
 curve_df = pd.read_excel('线路条件数据.xlsx', sheet_name='curve')
 
+static_limit_x = []
+static_limit_y = []
 def plot_static_limit_curve():
     static_limit_segments: List[Tuple[Tuple[float, float], Tuple[float, float]]] = []
     for i in range(len(station_df.index)):
@@ -19,25 +36,44 @@ def plot_static_limit_curve():
         static_limit_segments.append(((x1, y1), (x2, y2)))
     static_limit_segments = sorted(static_limit_segments, key=lambda x: x[0][0])
 
-    static_limit_points: List[Tuple[float, float]] = [(0, 87)]
+    static_limit_points: List[Tuple[float, float]] = [(0, max_speed)]
     x = 0
     for segment in static_limit_segments:
         p1, p2 = segment
         if x < p1[0]:
-            static_limit_points.append((x, 87))
-            static_limit_points.append((p1[0], 87))
+            static_limit_points.append((x, max_speed))
+            static_limit_points.append((p1[0], max_speed))
         static_limit_points.append(p1)
         static_limit_points.append(p2)
         x = p2[0]
-    if x < 24000:
-        static_limit_points.append((x, 87))
-        static_limit_points.append((24000, 87))
+    if x < max_distance:
+        static_limit_points.append((x, max_speed))
+        static_limit_points.append((max_distance, max_speed))
 
     static_limit_points_x = [point[0] for point in static_limit_points]
     static_limit_points_y = [point[1] for point in static_limit_points]
-    plt.plot(static_limit_points_x, static_limit_points_y, 'b-')
+    interp_func = interp1d(static_limit_points_x, static_limit_points_y, kind='linear')
+    global static_limit_x, static_limit_y
+    static_limit_x = np.linspace(0, max_distance, max_distance * 100)
+    static_limit_y = interp_func(static_limit_x)
 
-    plt.xlabel('Distance (m)')
-    plt.ylabel('Speed Limit (km/h)')
-    plt.title('Speed Limit Curve')
+ebi_x, ebi_y = [], []
+def plot_ebi():
+    global static_limit_x, static_limit_y
+    global ebi_x, ebi_y
+    x, y = static_limit_x.copy(), static_limit_y.copy()
+    delta_x = x[1] - x[0]
+    for i in range(2, len(y)+1):
+        safev = np.sqrt(2 * 紧急制动率 * delta_x + y[-i + 1] ** 2)
+        y[-i] = min(y[-i], safev)
+
+    ebi_x, ebi_y = x, y
+
+
+if __name__ == '__main__':
+    plot_static_limit_curve()
+    plt.plot(static_limit_x, static_limit_y, 'b-')
+
+    plot_ebi()
+    plt.plot(ebi_x, ebi_y, 'r-')
     plt.show()
