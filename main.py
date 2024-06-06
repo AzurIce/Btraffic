3893ae64fdb25807e847163bd8e8c979a039b030
Azur冰弦 <973562770@qq.com>
2024-06-06 17:04:31 +0800

    feat: speed curve with stops

--- main.py
+++ main.py
@@ -2,55 +2,15 @@ import pandas as pd
 # import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 import numpy as np
+from plotly.subplots import make_subplots
 from scipy.interpolate import interp1d
 
 from typing import Callable, List, Sequence, Tuple
 
-# 启动加速度 = 0.8 # m/s^2
-# 牵引加速度 = 0.5 # m/s^2
-# 紧急制动率 = 1.2 # m/s^2
-# 常用制动率 = 0.5 # m/s^2
-# 车长 = 80 # m
-# 制动建立时延 = 0.7 # s
-# 牵引切断延时 = 0.7 # s
-# 旋转质量系数 = 1.08
-# ATP余量 = 3 # km/h
-# ATO余量 = 5 # km/h
-# max_distance: float = 24000 # m
-# max_speed = 87 # km/h
-g = 9.81
-# m = 800 * 10 ** 3 # kg
-
-# Load the Excel file using pandas
-FILE_NAME = '线路条件数据.xlsx'
-basic_info_df = pd.read_excel(FILE_NAME, sheet_name='BasicInfo')
-station_df = pd.read_excel(FILE_NAME, sheet_name='station')
-curve_df = pd.read_excel(FILE_NAME, sheet_name='curve')
-grad_df = pd.read_excel(FILE_NAME, sheet_name='grad')
-
-启动加速度 = basic_info_df.iloc[0, 0] # m/s^2
-牵引加速度 = basic_info_df.iloc[0, 1] # m/s^2
-紧急制动率 = basic_info_df.iloc[0, 2] # m/s^2
-常用制动率 = basic_info_df.iloc[0, 3] # m/s^2
-车长 = basic_info_df.iloc[0, 4] # m
-制动建立时延 = basic_info_df.iloc[0, 5] # s
-牵引切断延时 = basic_info_df.iloc[0, 6] # s
-旋转质量系数 = basic_info_df.iloc[0, 7]
-ATP余量 = basic_info_df.iloc[0, 8] # km/h
-ATO余量 = basic_info_df.iloc[0, 9] # km/h
-max_distance = basic_info_df.iloc[0, 10] # m
-max_speed = basic_info_df.iloc[0, 11] # km/h
-
-# length: x_scale * max_distance
-# index: [0, xscale * max_distance)
-# value: [0, max_distance]
-x_scale = 1
-X = np.arange(0, max_distance, 1 / x_scale).tolist()
-
 def grad_a(grad, k):
     # grad_degree = np.arctan(grad / 1000)
     # return g * np.sin(grad_degree) / k
-    return grad / 1000 / k
+    return grad / 100 / k
 
 def sign(x):
     return 1 if x == 1 else -1
@@ -60,7 +20,7 @@ def read_segments(
     get_x1: Callable[[pd.DataFrame, int], float],
     get_x2: Callable[[pd.DataFrame, int], float],
     get_y: Callable[[pd.DataFrame, int], float],
-):
+) -> List[Tuple[Tuple[float, float], Tuple[float, float]]]:
     '''read segments from df'''
     segments = []
     for i in range(len(df.index)):
@@ -74,7 +34,7 @@ def read_segments(
 def segments_to_points(
     segments: List[Tuple[Tuple[float, float], Tuple[float, float]]],
     default_y: float=0
-):
+) -> List[float]:
     '''convert segments to points'''
     points: List[Tuple[float, float]] = [(0, default_y)]
     x = 0
@@ -96,6 +56,56 @@ def segments_to_points(
 
     return interp_func(X)
 
+# TODO: 第一个站的终端作为发车起点
+stops = [1, 6, 10, 20]
+
+# 启动加速度 = 0.8 # m/s^2
+# 牵引加速度 = 0.5 # m/s^2
+# 紧急制动率 = 1.2 # m/s^2
+# 常用制动率 = 0.5 # m/s^2
+# 车长 = 80 # m
+# 制动建立时延 = 0.7 # s
+# 牵引切断延时 = 0.7 # s
+# 旋转质量系数 = 1.08
+# ATP余量 = 3 # km/h
+# ATO余量 = 5 # km/h
+# max_distance: float = 24000 # m
+# max_speed = 87 # km/h
+g = 9.81
+# m = 800 * 10 ** 3 # kg
+
+# Load the Excel file using pandas
+FILE_NAME = '线路条件数据.xlsx'
+basic_info_df = pd.read_excel(FILE_NAME, sheet_name='BasicInfo')
+station_df = pd.read_excel(FILE_NAME, sheet_name='station')
+station_segments = read_segments(
+    station_df,
+    lambda df, i: df.iloc[i, 0],
+    lambda df, i: df.iloc[i, 1],
+    lambda df, i: df.iloc[i, 2]
+)
+
+curve_df = pd.read_excel(FILE_NAME, sheet_name='curve')
+grad_df = pd.read_excel(FILE_NAME, sheet_name='grad')
+
+启动加速度 = basic_info_df.iloc[0, 0] # m/s^2
+牵引加速度 = basic_info_df.iloc[0, 1] # m/s^2
+紧急制动率 = basic_info_df.iloc[0, 2] # m/s^2
+常用制动率 = basic_info_df.iloc[0, 3] # m/s^2
+车长 = basic_info_df.iloc[0, 4] # m
+制动建立时延 = basic_info_df.iloc[0, 5] # s
+牵引切断延时 = basic_info_df.iloc[0, 6] # s
+旋转质量系数 = basic_info_df.iloc[0, 7]
+ATP余量 = basic_info_df.iloc[0, 8] # km/h
+ATO余量 = basic_info_df.iloc[0, 9] # km/h
+max_distance = basic_info_df.iloc[0, 10] # m
+max_speed = basic_info_df.iloc[0, 11] # km/h
+
+# length: x_scale * max_distance
+# index: [0, xscale * max_distance)
+# value: [0, max_distance]
+x_scale = 1
+X = np.arange(0, max_distance, 1 / x_scale).tolist()
 
 def calc_grad_a() -> List[float]:
     grad_a_segments = read_segments(
@@ -124,8 +134,11 @@ def calc_static_limit() -> List[float]:
     return segments_to_points(static_limit_segments, default_y=max_speed) # km/h
 
 def calc_v(sbi_y, extra_a) -> List[float]:
-    v: List[float] = [0]
-    for i in range(len(X) - 1):
+    start = int(station_segments[0][0][0] * x_scale)
+    end = int(station_segments[-1][1][0] * x_scale)
+
+    v: List[float] = [0 for i in range(start + 1)]
+    for i in range(start, end + 1):
         a = 启动加速度 if v[i] < 40 * 10 ** 3 else 牵引加速度
         newv = np.sqrt(2 * (a + extra_a[i]) * (1 / x_scale) + v[i] ** 2)
         if newv > sbi_y[i]:
@@ -187,24 +200,47 @@ if __name__ == '__main__':
 
     extra_a = calc_grad_a()
     ebi_y = calc_bi(static_limit_kmh, 紧急制动率, ATP余量, extra_a)
+
     sbi_y = calc_bi(static_limit_kmh, 常用制动率, ATO余量, extra_a)
+    sbi_y = [x / 3.6 for x in sbi_y]
+    for stop in stops:
+        station_start, station_end = station_segments[stop]
+
+        x = int(station_end[0])
+        sbi_y[x] = 0
+        x -= 1
+        while x >= 0:
+            v = np.sqrt(2 * (常用制动率 + extra_a[x]) * (1 / x_scale) + sbi_y[x+1] ** 2)
+            if sbi_y[x] < v:
+                break
+            sbi_y[x] = v
+            x -= 1
+    sbi_y = [x * 3.6 for x in sbi_y]
+
     v_y = calc_v(sbi_y, extra_a)
 
     fig = go.Figure(data=[
         go.Scatter(x=X, y=static_limit_kmh, name='静态限速'),
-        go.Scatter(x=X, y=extra_a, name='坡度加速度'),
         go.Scatter(x=X, y=ebi_y, name='EBI'),
         go.Scatter(x=X, y=sbi_y, name='SBI'),
-        go.Scatter(x=X, y=v_y, name='列车运行速度')
+        go.Scatter(x=X, y=v_y, name='列车运行速度'),
+        go.Scatter(x=X, y=extra_a, name='坡度加速度'),
     ])
+
     fig.update_layout(
         title='作业罢了',
         xaxis_title='Distance (m)',
         yaxis_title='Speed (km/h)',
         xaxis=dict(
+            range=[station_segments[0][0][0], station_segments[-1][1][0]],
             rangeslider=dict(
+                range=[station_segments[0][0][0], station_segments[-1][1][0]],
                 visible=True
             )
-        )
+        ),
+        shapes=[
+            dict(type="rect", x0=s[0][0], x1=s[1][0], y0=0, y1=90, layer="below", fillcolor="lightblue", opacity=0.5)
+            for s in station_segments
+        ]
     )
     fig.show()
