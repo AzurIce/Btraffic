a47ea21e7140bd82d38befa11e036b5cc7434970
Azur冰弦 <973562770@qq.com>
2024-06-06 15:28:41 +0800

    refactor: correct ebi and sbi calculation

--- README.md
+++ README.md
@@ -6,23 +6,70 @@ pip install pandas matplotlib openpyxl numpy scipy plotly
 
 
 
-位置为 $x$ 时速度为 $v_m$
+进行紧急制动的过程中由于「牵引切断时延」和「制动建立时延」会导致存在三个阶段：
 
+1. 牵引：加速度为 **加速度**，持续 $t_0$
+2. 切断牵引，仍未建立制动：加速度为 $0$，持续 $t_1$
+3. 制动：加速度为 **制动率**
+
+
+
+**先推出一条只考虑 3 的曲线**
+
+在任意一个点 $x_1$，我们可以逆推绘制出以该点为终点的 **制动** 速度曲线：
 $$
-2ax = v(x_1)^2 - v(x_0)^2
+v(x_1-\delta_x) = \sqrt{-2a\delta_x + v(x_1)^2}
+$$
+将这条曲线与限速曲线取最小即可得到满足该点安全的制动安全曲线。从后往前对所有点进行重复，即可得到整体的制动安全曲线。
+
+然而在实际实现上，数据是离散的采样点，而非连续直线，计算机也没有两条曲线取最小的直觉，只能比较同一 $x$ 处的 $y$。
+
+**然后由这条曲线作为输入，考虑 1 和 2，再推出最终的曲线**
+
+假设对于 $x > x_0$ 的速度均已得到，那么 $x_0$ 处的速度 $v(x_0)$ 受到其后 $\delta_t \in [0, t_0 + t_1]$ 时间内的速度的限制：
+$$
+v(x_0) = \min \begin{cases}
+    v(x_0 + \delta_x) - a\delta_t, &0 \leq \delta_t < t_0\\
+    v(x_0 + \delta_x) - at_0, &t_0 \leq \delta_t \leq t_0 + t_1\\
+\end{cases}
+$$
+或者说如果我们已知 $x_1$ 处的速度 $v(x_1)$，那么它会限制其前 $\delta_t \in [0, t_0 + t_1]$ 内的速度：
+$$
+v(x_1 - \delta_x) \leq \begin{cases}
+	v(x_1), & 0 \leq \delta_t < t_0\\
+	v(x_1) - a(\delta_t - t_0), & t_0 \leq \delta_t \leq t_0 + t_1
+\end{cases}
+$$
+现在我们希望能够用 $\delta_x$ 来表示 $\delta_t$，可以转化为：
+$$
+v(x_1 - \delta_x) \leq \begin{cases}
+	v(x_1), & 0 \leq \delta_x < v(x_1)t_1\\
+	\sqrt{-2a(\delta_x - v(x_1)t_1) + v(x_1)^2}, & v(x_1)t_1 \leq \delta_x \leq t_1 + v(x_1)t_0 - \frac{1}{2}at_0^2
+\end{cases}
 $$
 
-$$
-\sqrt{-2ax + v(x_1)^2} = v(x_0)
-$$
+用这个式子逆着再递推一边就好了。
 
-$$
-x_0 = x_1 - \delta
-$$
 
+
+下面这个不对
+
+
+在这个基础之上，可以再加入 1、2 的考虑，逆推 $\Delta t = t_0 + t_1$ 时间前，$x_0$ 处的速度 $v(x_0)$：
 $$
-\sqrt{-2a\delta + v(x_1)^2} = v(x_1-\delta)
+\begin{align}
+x_0
+&= x_1 - [v_1t_1] - [(v_1 - at_0)t_0 + \frac{1}{2}at_0^2]\\
+&= x_1 - [v_1t_1] - [v_1t_0 - \frac{1}{2}at_0^2]\\
+
+v(x_0) &= v_0 = v(x_1) - at_0 = v_1 - at_0
+\end{align}
 $$
+即 **整个制动过程** 的底线。
+
+然而使用 $\Delta t$ 会导致逆推过程中 $x$ 落点不重叠的问题。
+
+
 
 
 
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import plotly.graph_objects as go
 import numpy as np
 from scipy.interpolate import interp1d
 
-from typing import List, Tuple
+from typing import Callable, List, Sequence, Tuple
 
 启动加速度 = 0.8 # m/s^2
 牵引加速度 = 0.5 # m/s^2
@@ -16,13 +16,16 @@ from typing import List, Tuple
 旋转质量系数 = 1.08
 ATP余量 = 3 # km/h
 ATO余量 = 5 # km/h
-max_distance = 24000 # m
+max_distance: float = 24000 # m
 max_speed = 87 # km/h
 g = 9.81
-m = 800 * 10 ** 3 # kg
+# m = 800 * 10 ** 3 # kg
 
-x_step = 1
-X = np.linspace(0, max_distance, int(max_distance / x_step)).tolist()
+# length: x_scale * max_distance
+# index: [0, xscale * max_distance)
+# value: [0, max_distance]
+x_scale = 1
+X = np.arange(0, max_distance, 1 / x_scale).tolist()
 
 # Load the Excel file using pandas
 FILE_NAME = '线路条件数据.xlsx'
@@ -38,120 +41,142 @@ def grad_a(grad, k):
 def sign(x):
     return 1 if x == 1 else -1
 
-def calc_grad_a() -> List[float]:
-    grad_a_segments: List[Tuple[Tuple[float, float], Tuple[float, float]]] = []
-    for i in range(len(grad_df.index)):
-        x1, y1 = grad_df.iloc[i, 0], grad_df.iloc[i, 2] * grad_a(sign(grad_df.iloc[i, 3]), 旋转质量系数)
-        x2, y2 = grad_df.iloc[i, 1], grad_df.iloc[i, 2] * grad_a(sign(grad_df.iloc[i, 3]), 旋转质量系数)
-        grad_a_segments.append(((x1, y1), (x2, y2)))
-    grad_a_segments = sorted(grad_a_segments, key=lambda x: x[0][0])
+def read_segments(
+    df: pd.DataFrame,
+    get_x1: Callable[[pd.DataFrame, int], float],
+    get_x2: Callable[[pd.DataFrame, int], float],
+    get_y: Callable[[pd.DataFrame, int], float],
+):
+    '''read segments from df'''
+    segments = []
+    for i in range(len(df.index)):
+        x1, x2 = get_x1(df, i), get_x2(df, i)
+        y = get_y(df, i)
+        segments.append(((x1, y), (x2, y)))
+    segments = sorted(segments, key=lambda x: x[0][0])
+    return segments
 
-    points: List[Tuple[float, float]] = [(0, 0)]
+
+def segments_to_points(
+    segments: List[Tuple[Tuple[float, float], Tuple[float, float]]],
+    default_y: float=0
+):
+    '''convert segments to points'''
+    points: List[Tuple[float, float]] = [(0, default_y)]
     x = 0
-    for segment in grad_a_segments:
+    for segment in segments:
         p1, p2 = segment
         if x < p1[0]:
-            points.append((x, 0))
-            points.append((p1[0], 0))
+            points.append((x, default_y))
+            points.append((p1[0], default_y))
         points.append(p1)
         points.append(p2)
         x = p2[0]
     if x < max_distance:
-        points.append((x, 0))
-        points.append((max_distance, 0))
+        points.append((x, default_y))
+        points.append((max_distance, default_y))
 
     points_x = [point[0] for point in points]
     points_y = [point[1] for point in points]
     interp_func = interp1d(points_x, points_y, kind='linear')
 
-    y = interp_func(X)
-    return y
+    return interp_func(X)
+
+
+def calc_grad_a() -> List[float]:
+    grad_a_segments = read_segments(
+        grad_df,
+        lambda df, i: df.iloc[i, 0],
+        lambda df, i: df.iloc[i, 1],
+        lambda df, i: df.iloc[i, 2] * grad_a(sign(df.iloc[i, 3]), 旋转质量系数)
+    )
+    return segments_to_points(grad_a_segments)
 
 def calc_static_limit() -> List[float]:
-    static_limit_segments: List[Tuple[Tuple[float, float], Tuple[float, float]]] = []
-    for i in range(len(station_df.index)):
-        x1, y1 = station_df.iloc[i, 0], station_df.iloc[i, 2]
-        x2, y2 = station_df.iloc[i, 1], station_df.iloc[i, 2]
-        static_limit_segments.append(((x1, y1), (x2, y2)))
-    for i in range(len(curve_df.index)):
-        x1, y1 = curve_df.iloc[i, 0], curve_df.iloc[i, 7]
-        x2, y2 = curve_df.iloc[i, 1], curve_df.iloc[i, 7]
-        static_limit_segments.append(((x1, y1), (x2, y2)))
+    static_limit_segments = []
+    static_limit_segments += read_segments(
+        station_df,
+        lambda df, i: df.iloc[i, 0],
+        lambda df, i: df.iloc[i, 1],
+        lambda df, i: df.iloc[i, 2]
+    )
+    static_limit_segments += read_segments(
+        curve_df,
+        lambda df, i: df.iloc[i, 0],
+        lambda df, i: df.iloc[i, 1],
+        lambda df, i: df.iloc[i, 7]
+    )
     static_limit_segments = sorted(static_limit_segments, key=lambda x: x[0][0])
-
-    points: List[Tuple[float, float]] = [(0, max_speed)]
-    x = 0
-    for segment in static_limit_segments:
-        p1, p2 = segment
-        if x < p1[0]:
-            points.append((x, max_speed))
-            points.append((p1[0], max_speed))
-        points.append(p1)
-        points.append(p2)
-        x = p2[0]
-    if x < max_distance:
-        points.append((x, max_speed))
-        points.append((max_distance, max_speed))
-
-    points_x = [point[0] for point in points]
-    points_y = [point[1] for point in points]
-    interp_func = interp1d(points_x, points_y, kind='linear')
-
-    y = interp_func(X)
-    return y
-
-def calc_safev(static_limit_y, a, redundant=0) -> List[float]:
-    y = [y - redundant for y in static_limit_y]
-    for i in range(2, len(y)+1):
-        safev = np.sqrt(2 * a * x_step + y[-i + 1] ** 2)
-        y[-i] = min(y[-i], safev)
-
-    return y
-
-def calc_safev_with_extra_a(static_limit_y, extra_a, a, redundant=0) -> List[float]:
-    y = [y - redundant for y in static_limit_y]
-    for i in range(2, len(y)+1):
-        safev = np.sqrt(max(2 * (a + extra_a[-i]) * x_step + y[-i + 1] ** 2, 0))
-        y[-i] = min(y[-i], safev)
-
-    return y
+    return segments_to_points(static_limit_segments, default_y=max_speed) # km/h
 
 def calc_v(sbi_y, extra_a) -> List[float]:
     v: List[float] = [0]
     for i in range(len(X) - 1):
         a = 启动加速度 if v[i] < 40 * 10 ** 3 else 牵引加速度
-        newv = np.sqrt(2 * (a + extra_a[i]) * x_step + v[i] ** 2)
+        newv = np.sqrt(2 * (a + extra_a[i]) * (1 / x_scale) + v[i] ** 2)
         if newv > sbi_y[i]:
             newv = sbi_y[i]
         v.append(newv)
     return v
 
+def calc_safev(static_limit_ms, a, extra_a=[0 for i in X]) -> List[float]:
+    y = static_limit_ms.copy()
+    for i in range(2, len(y)+1):
+        safev = np.sqrt(max(2 * (a + extra_a[-i]) * (1 / x_scale) + y[-i + 1] ** 2, 0))
+        y[-i] = min(y[-i], safev)
+
+    return y
+
+def calc_bi(static_limit_kmh, break_a, redundant, extra_a=[0 for i in X]) -> List[float]:
+    accel_a = 牵引加速度
+    # break_a = 紧急制动率
+    t0 = 牵引切断延时
+    t1 = 制动建立时延
+    # redundant = ATP余量
+    x_step = 1 / x_scale
+
+    y = [(x - redundant) / 3.6 for x in static_limit_kmh] # add ato offset and convert from km/h to m/s
+
+    # 制动建立
+    safev = calc_safev(y, break_a, extra_a)
+    y = safev.copy()
+
+    # 考虑延时
+    for i in range(1, len(X) + 1):
+        v1 = safev[-i]
+
+        mid_delta_x = v1 * t1
+        max_delta_x = mid_delta_x + v1 * t0 - 0.5 * accel_a * t0 * t0
+
+        for step in range(1, int(mid_delta_x / x_step) + 1):
+            if i + step > len(X):
+                break
+            y[-i - step] = min(y[-i - step], v1)
+
+        for step in range(int(mid_delta_x / x_step) + 1, int(max_delta_x / x_step) + 1):
+            if i + step > len(X):
+                break
+            delta_x = step * x_step
+            v = np.sqrt(-2 * accel_a * (delta_x - mid_delta_x) + v1 ** 2)
+            y[-i - step] = min(y[-i - step], v)
+
+    return [x * 3.6 for x in y]
+
+
 if __name__ == '__main__':
-    x = X
-    static_limit_y = calc_static_limit()
+    static_limit_kmh = calc_static_limit()
 
     extra_a = calc_grad_a()
-    # plt.plot(x, extra_a)
-    # print(extra_a)
-    # ebi_x, ebi_y = calc_safev(x, static_limit_y, 紧急制动率)
-    # sbi_x, sbi_y = calc_safev(x, static_limit_y, 常用制动率)
-    # plt.plot(x, static_limit_y, 'b-')
-    # plt.plot(ebi_x, ebi_y, 'r-')
-    # plt.plot(sbi_x, sbi_y, 'g-')
-    # plt.show()
-
-    ebi_y = calc_safev_with_extra_a(static_limit_y, extra_a, 紧急制动率)
-    sbi_y = calc_safev_with_extra_a(static_limit_y, extra_a, 常用制动率, ATO余量)
+    ebi_y = calc_bi(static_limit_kmh, 紧急制动率, ATP余量, extra_a)
+    sbi_y = calc_bi(static_limit_kmh, 常用制动率, ATO余量, extra_a)
     v_y = calc_v(sbi_y, extra_a)
-    # plt.plot(x, static_limit_y, 'b-')
-    # plt.plot(ebi_x, ebi_y, 'r-')
-    # plt.plot(sbi_x, sbi_y, 'g-')
-    # plt.show()
+
     fig = go.Figure(data=[
-        go.Scatter(x=x, y=static_limit_y, name='静态限速'),
-        go.Scatter(x=x, y=ebi_y, name='EBI'),
-        go.Scatter(x=x, y=sbi_y, name='SBI'),
-        go.Scatter(x=x, y=v_y, name='列车运行速度')
+        go.Scatter(x=X, y=static_limit_kmh, name='静态限速'),
+        go.Scatter(x=X, y=extra_a, name='坡度加速度'),
+        go.Scatter(x=X, y=ebi_y, name='EBI'),
+        go.Scatter(x=X, y=sbi_y, name='SBI'),
+        go.Scatter(x=X, y=v_y, name='列车运行速度')
     ])
     fig.update_layout(
         title='作业罢了',
